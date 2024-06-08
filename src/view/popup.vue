8a70dc9fd2ab1a63ac3ecbf9592fe3fa26f0a488
Alimoustafa0909 <alimoustafaprogram@gmail.com>
2024-06-08 15:59:28 +0300

    Testing the Code

---
+++ src/view/popup.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <iframe
+      src="http://localhost:8080/"
+height="200" width="300"
+      frameborder="300"
+     >
+    </iframe>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    return {};
+  }
+});
+</script>
+
+<style scoped>
+#app {
+  text-align: center;
+}
+
+iframe {
+  border: none;
+}
+</style>
